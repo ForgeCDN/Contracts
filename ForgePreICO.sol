654a4cc3fb838c70e65b73c8c656223cb0e15079
Bixente <bixente3669@gmail.com>
2018-04-01 23:00:32 +0700

    Move contracts from bitbucket

---
+++ ForgePreICO.sol
@@ -0,0 +1,110 @@
+pragma solidity ^0.4.18;
+
+import "./SafeMath.sol";
+import "./ERC20.sol";
+
+/**
+* @title ForgePreICO
+* @dev Contract to preSale ForgeCDN tokens.
+*/
+contract ForgePreICO {
+    using SafeMath for uint256;
+
+    // The token being sold
+    ERC20 public token;
+    // start and end timestamps where investments are allowed (both inclusive)
+    uint256 public startTime;
+    uint256 public endTime;
+    // address where funds are collected
+    address public wallet;
+    // how many token units a buyer gets per wei
+    uint256 public rate;
+    // amount of raised money in wei
+    uint256 public weiRaised;
+    // Hardcap
+    uint256 public cap;
+    // Operation wallet
+    address public operationWallet;
+    // Vault wallet
+    address public vault;
+
+    /**
+     * event for token purchase logging
+     * @param purchaser who paid for the tokens
+     * @param beneficiary who got the tokens
+     * @param value weis paid for purchase
+     * @param amount amount of tokens purchased
+     */
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+    function ForgePreICO(address _wallet, ERC20 _token, address _vault) public
+    {
+        require(_wallet != address(0));
+        require(_vault != address(0));
+        require(_token != address(0));
+        operationWallet = 0xcbe38e3b2a98d664890e8f358edb8071e20f5188;
+        //wallet = 0x1B8C56f79A59320f40aB9d7E7CcA1eC5D8fCF1a0;
+        cap = 2400 * 1 ether;
+        startTime = 1522670400;
+        endTime = 1525003200;
+        rate = 1500;
+        wallet = _wallet;
+        vault = _vault;
+        token = _token;
+    }
+
+    /**
+     * fallback function to buy tokens
+     */
+    function() external payable {
+        address beneficiary = msg.sender;
+        require(beneficiary != address(0));
+        require(validPurchase());
+        uint256 weiAmount = msg.value;
+
+        // calculate token amount to be send
+        uint256 tokens = weiAmount.mul(rate);
+        uint256 operationTokens = tokens.div(69).mul(11);
+        uint256 commandTokens = tokens.div(69).mul(20);
+        
+        // update state
+        weiRaised = weiRaised.add(weiAmount);
+
+        token.transferFrom(wallet, beneficiary, tokens);
+        token.transferFrom(wallet, operationWallet, operationTokens);
+        token.transferFrom(wallet, vault, commandTokens);
+        
+        TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+        forwardFunds();
+    }
+
+    /**
+     * @return true if crowdsale event has ended
+     */
+    function hasEnded() public view returns (bool) {
+        bool capReached = weiRaised >= cap;
+        bool timeReached = now > endTime;
+        return capReached || timeReached;
+    }
+
+    /**
+     * send ether to the fund collection wallet
+     */
+    function forwardFunds() internal {
+        wallet.transfer(msg.value);
+    }
+
+
+    /**
+     * @return true if the transaction can buy tokens
+     */
+    function validPurchase() internal view returns (bool) {
+        bool withinPeriod = now >= startTime && now <= endTime;
+        bool nonZeroPurchase = msg.value != 0;
+        bool standardValidation = withinPeriod && nonZeroPurchase;
+        bool withinCap = weiRaised.add(msg.value) <= cap;
+        return withinCap && standardValidation;
+    }
+
+}
---
+++ ForgeVault.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.4.18;
+
+import "./ERC20.sol";
+import "./SafeMath.sol";
+
+contract ForgeVault {
+    using SafeMath for uint256;
+
+    uint256 firstFunding = 1534334400;
+    uint256 secondFunding = 1565870400;
+    address public owner;
+    address public pendingOwner;
+    ERC20 public ForgeToken;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Modifier throws if called by any account other than the pendingOwner.
+     */
+    modifier onlyPendingOwner() {
+        require(msg.sender == pendingOwner);
+        _;
+    }
+
+    function ForgeVault(ERC20 _ForgeToken){
+        owner = msg.sender;
+        ForgeToken = _ForgeToken;
+    }
+
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        pendingOwner = newOwner;
+    }
+    /**
+     * @dev Allows the pendingOwner address to finalize the transfer.
+     */
+    function claimOwnership() public onlyPendingOwner {
+        OwnershipTransferred(owner, pendingOwner);
+        owner = pendingOwner;
+        pendingOwner = address(0);
+    }
+
+    /**
+     * @dev This method requires that today it is later than August 15, 2018
+     */
+    function firstWithdraw() public onlyOwner {
+        require(now > firstFunding);
+        uint256 count = ForgeToken.balanceOf(this).div(2);
+        ForgeToken.transfer(owner, count);
+    }
+
+    /**
+     * @dev This method requires that today it is later than August 15, 2019
+     */
+    function secondWithdraw() public onlyOwner {
+        require(now > secondFunding);
+        uint256 count = ForgeToken.balanceOf(this);
+        ForgeToken.transfer(owner, count);
+    }
+
+}
